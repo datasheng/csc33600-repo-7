7cd3df0702fa717fa5eeb90a2dc2aae89352ebb5
Codingologist <FemiAdeon@gmail.com>
2025-05-04 13:27:01 -0400

    update sql code

--- BestElectronics4U/makeuserpaid.sql
+++ BestElectronics4U/makeuserpaid.sql
@@ -2,9 +2,9 @@
 
 UPDATE user
 SET paid_user = TRUE
-WHERE email = 'FemiAdeon@gmail.com'; -- or use WHERE email = 'user@example.com'
+WHERE email = 'example@gmail.com'; -- or use WHERE email = 'user@example.com'
 
 
 SELECT user_id, email, paid_user
 FROM electronics4u.user
-WHERE email = 'FemiAdeon@gmail.com';
+WHERE email = 'example@gmail.com';
